33a2cfefcc2f04ba41ca45435e3a87f5a36de4e7
DevY <hanys1042@gmail.com>
2022-09-18 15:08:19 +0900

    table sql

--- create.sql
+++ create.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS publisher (
-    publisher_no    INTEGER PRIMARY KEY AUTOINCREMENT,
-    name            TEXT NOT NULL,
-    website         TEXT NOT NULL,
-    rss_url         TEXT NOT NULL,
-    reg_date        TEXT NOT NULL,
-    mod_date        TEXT NOT NULL
+    publisher_no    INTEGER     PRIMARY KEY AUTOINCREMENT,
+    name            TEXT        NOT NULL,
+    website         TEXT        NOT NULL,
+    rss_url         TEXT        NOT NULL,
+    reg_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    mod_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 
 );
 
 CREATE TABLE IF NOT EXISTS news (
-    news_no     INTEGER PRIMARY KEY AUTOINCREMENT,
-    title       TEXT NOT NULL,
-    link        TEXT NOT NULL,
-    description TEXT NOT NULL,
-    pub_date    TEXT NOT NULL,
-    image       TEXT NOT NULL,
-    author      TEXT NOT NULL,
-    reg_date    TEXT NOT NULL
+    news_no     INTEGER     PRIMARY KEY AUTOINCREMENT,
+    title       TEXT        NOT NULL,
+    link        TEXT        NOT NULL,
+    description TEXT        NOT NULL,
+    pub_date    TEXT        NOT NULL,
+    image       TEXT        NOT NULL,
+    author      TEXT        NOT NULL,
+    reg_date    TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
