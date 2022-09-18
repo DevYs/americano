24f65bec6aacd537c639d25e082e707cafee62ed
DevY <hanys1042@gmail.com>
2022-09-18 15:00:07 +0900

    table sql

--- create.sql
+++ create.sql
@@ -1,9 +1,20 @@
-CREATE TABLE IF NOT EXISTS exam (
-    exam_no     INTEGER PRIMARY KEY AUTOINCREMENT,
-    exam_name   TEXT
+CREATE TABLE IF NOT EXISTS publisher (
+    publisher_no    INTEGER PRIMARY KEY AUTOINCREMENT,
+    name            TEXT NOT NULL,
+    website         TEXT NOT NULL,
+    rss_url         TEXT NOT NULL,
+    reg_date        TEXT NOT NULL,
+    mod_date        TEXT NOT NULL
+
 );
 
-CREATE TABLE IF NOT EXISTS employee (
-    emp_no      INTEGER PRIMARY KEY AUTOINCREMENT,
-    emp_name    TEXT
+CREATE TABLE IF NOT EXISTS news (
+    news_no     INTEGER PRIMARY KEY AUTOINCREMENT,
+    title       TEXT NOT NULL,
+    link        TEXT NOT NULL,
+    description TEXT NOT NULL,
+    pub_date    TEXT NOT NULL,
+    image       TEXT NOT NULL,
+    author      TEXT NOT NULL,
+    reg_date    TEXT NOT NULL
 );
