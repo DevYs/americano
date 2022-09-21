4af9941880a4cab2d595e58de935d3856dded3c8
DevY <devy@DevYui-MacBookAir.local>
2022-09-21 17:54:42 +0900

    scheduler

--- src/main/java/devy/americano/backend/scheduler/NewsScheduler.java
+++ src/main/java/devy/americano/backend/scheduler/NewsScheduler.java
@@ -1,21 +1,34 @@
 package devy.americano.backend.scheduler;
 
+import devy.americano.backend.domain.News;
+import devy.americano.backend.service.NewsService;
+import devy.americano.backend.service.PublisherRssService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @EnableScheduling
+@RequiredArgsConstructor
 public class NewsScheduler {
 
     private final Logger logger = LoggerFactory.getLogger(NewsScheduler.class);
 
-    private final long FIXED_RATE_VALUE = 5 * 60 * 1000;
+    private final long FIXED_RATE_VALUE = 10 * 60 * 1000;
+
+    private final PublisherRssService publisherRssService;
+
+    private final NewsService newsService;
 
     @Scheduled(fixedRate = FIXED_RATE_VALUE)
     public void run() {
+        List<News> newsList = publisherRssService.rss();
+        newsService.insertNewsList(newsList);
     }
 
 }
