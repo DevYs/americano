ead461c56f37fc56ee062641844db2141a8c37bd
DevY <hanys1042@gmail.com>
2022-09-27 16:27:07 +0900

    cors 설정 추가

---
+++ src/main/java/devy/americano/backend/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package devy.americano.backend.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@AllArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PATCH.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+}
