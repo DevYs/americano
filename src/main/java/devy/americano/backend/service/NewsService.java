de375de3ba71db6e7850c27428907460e5295cbb
DevY <hanys1042@gmail.com>
2022-09-29 17:47:43 +0900

    뉴스 삭제시 UTC 시간 적용

--- src/main/java/devy/americano/backend/service/NewsService.java
+++ src/main/java/devy/americano/backend/service/NewsService.java
@@ -39,7 +39,7 @@ public class NewsService {
     }
 
     public void removeOldNews() {
-        LocalDateTime date = LocalDateTime.now().minusHours(5);
+        LocalDateTime date = LocalDateTime.now().minusHours(9 + 5);
         newsMapper.deleteOldNews(date);
     }
 
